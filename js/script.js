749700c6a941dd32c818eb0e512789e39b810167
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2025-01-28 12:53:33 +0100

    feat: es2 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//* Snack 1
+console.log("Snack 1");
+
 const carBrand = ["Aston Martin", "Audi", "Porsche", "Rolls Royce", "Bentley"];
 const carModel = ["Vanquish", "RS7", "GT3RS", "Spectre", "Continental"];
 const carPower = ["benzina", "diesel", "elettrico", "gpl", "metano"];
@@ -10,9 +13,9 @@ let alternativePower = [];
 // Creazione dell'array composto da 10 automobili
 for (let index = 0; index < 10; index++) {
     cars.push({
-        brand: brandRandom(),
-        model: modelRandom(),
-        power: powerRandom()
+        brand: brandRandom(carBrand),
+        model: modelRandom(carModel),
+        power: powerRandom(carPower)
     })
 }
 
@@ -38,23 +41,50 @@ console.log("Automobili a diesel:", diesel);
 console.log("Automobili ad alimentazione alternativa:", alternativePower);
 
 // Generazione randomica dei valori
-function brandRandom() {
+function brandRandom(brand) {
     const marcaIndex = Math.floor(Math.random() * carBrand.length);
-    const marca = carBrand[marcaIndex];
-    return marca;
+    return brand[marcaIndex];
     // console.log(marca);
 }
 
-function modelRandom() {
+function modelRandom(model) {
     const modelloIndex = Math.floor(Math.random() * carModel.length);
-    const modello = carModel[modelloIndex];
-    return modello;
+    return model[modelloIndex];
     // console.log(modello);
 }
 
-function powerRandom() {
+function powerRandom(power) {
     const alimentazioneIndex = Math.floor(Math.random() * carPower.length);
-    const alimentazione = carPower[alimentazioneIndex];
-    return alimentazione;
+    return power[alimentazioneIndex];
     // console.log(alimentazione);
 }
+
+//* Snack 2
+console.log("Snack 2");
+
+const stringArr = ["bEntLEY", "asTOn MartIn", "pOrsCHe"];
+const formattedStringArr = [];
+
+for (let index = 0; index < stringArr.length; index++) {
+    let word = stringArr[index];
+
+    // Riformatto tutte le stringhe in minuscolo
+    word = word.toLowerCase();
+    
+    // Prendo la prima lettera di ogni stringa e la formatto in maiuscola
+    // salvandola in una variabile
+    let firstLetter = word.charAt(0).toUpperCase();
+
+    // Sostituisco la prima lettera minuscola con quella maiuscola
+    word = word.replace(word.charAt(0), firstLetter)
+
+    // Oppure
+    // word = firstLetter + word.slice(1);
+
+    // Inserisco le stringhe formattate nel nuovo array
+    formattedStringArr.push(word);
+}
+
+console.log(stringArr);
+console.log(formattedStringArr);
+
